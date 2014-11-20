5df42b8c7a51d7dee4268ec8182842adfb6630c4
Zach Cardoza <bayssmekanique@live.com>
2014-11-20 13:02:26 -0800

    Adds NuGet Support

---
+++ nuspec/BootstrapContent/CryptoPluginBootstrap.cs.pp
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+ namespace $rootnamespace$.Bootstrap
+ {
+    public class CryptoPluginBootstrap
+        : MvxPluginBootstrapAction<Keiser.Mvx.Plugins.Crypto.PluginLoader>
+    {
+    }
+ }
---
+++ nuspec/BootstrapContent/CryptoTouchPluginBootstrap.cs.pp
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace $rootnamespace$.Bootstrap
+{
+    public class CryptoPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<Keiser.Mvx.Plugins.Crypto.PluginLoader, Keiser.Mvx.Plugins.Crypto>
+    {
+    }
+}
